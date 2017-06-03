7d82cc97cd97c0f26a2787773c351feffab3555f
Graham Jones <grahamjones139@gmail.com>
2017-06-03 23:51:25 +0100

    Added i2c driver - does not quite compile yet - issue with gpio inerrupt disable/enable

---
+++ osd_app/include/osd_app.h
@@ -0,0 +1,11 @@
+
+
+
+#ifndef __OSD_APP_H__
+#define __OSD_APP_H__
+#endif
+
+//void LEDBlinkTask(void *pvParam);
+
+
+
--- osd_app/user/user_main.c
+++ osd_app/user/user_main.c
@@ -26,8 +26,14 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
  */
-
+#include <esp_common.h>
+#include <i2c_master.h>
 #include "esp_common.h"
+#include "osd_app.h"
+
+#define DEV_ADDR 0x1D
+#define REG_ADDR 0x32
+
 
 /******************************************************************************
  * FunctionName : user_rf_cal_sector_set
@@ -77,6 +83,142 @@ uint32 user_rf_cal_sector_set(void)
 
     return rf_cal_sec;
 }
+
+/***************************************************/
+// i2c functions 
+void read_reg(int *result, uint8_t reg_addr) {
+	uint8_t ack = 1;
+	i2c_master_start();
+	//ack = i2c_master_writeByte((DEV_ADDR << 1) + 0);
+	i2c_master_writeByte((DEV_ADDR << 1) + 0);
+	if (!ack) {
+		printf("addr not ack when tx write command \n");
+		i2c_master_stop();
+	}
+	//ack = i2c_master_writeByte(reg_addr);
+	i2c_master_writeByte(reg_addr);
+	if (!ack) {
+		printf("register addr not ack \n");
+		i2c_master_stop();
+	}
+	os_delay_us(40000);
+	i2c_master_stop();
+	i2c_master_start();
+	//ack = i2c_master_writeByte((DEV_ADDR << 1) + 1);
+        i2c_master_writeByte((DEV_ADDR << 1) + 1);
+	if (!ack) {
+		printf("read device addr not ack when tx write command \n");
+		i2c_master_stop();
+	}
+	os_delay_us(40000);
+	int res = i2c_master_read();
+	i2c_master_stop();
+	*result = res;
+	printf("reading %x succeed\n", res);
+}
+
+void read_acc_reg(int32 client_sock, uint8_t reg_addr) {
+	uint8_t i, ack = 1; 
+	short x, y, z;
+	char buf[100];
+	short temp[6];
+	i2c_master_start();
+	//ack = i2c_master_writeByte((DEV_ADDR << 1) + 0);
+	i2c_master_writeByte((DEV_ADDR << 1) + 0);
+	if (!ack) {
+		printf("addr not ack when tx write command \n");
+		i2c_master_stop();
+	}
+	//ack = i2c_master_writeByte(reg_addr);
+	i2c_master_writeByte(reg_addr);
+	if (!ack) {
+		printf("register addr not ack \n");
+		i2c_master_stop();
+	}
+	os_delay_us(40000);
+	i2c_master_stop();
+	i2c_master_start();
+	//ack = i2c_master_writeByte((DEV_ADDR << 1) + 1);
+	i2c_master_writeByte((DEV_ADDR << 1) + 1);
+	if (!ack) {
+		printf("read device add not ack\n");
+		i2c_master_stop();
+	}
+	os_delay_us(40000);
+	for (i = 0; i < 6; ++i) {
+		temp[i] = i2c_master_read();
+		if (i == 5) {
+			i2c_set_ack(false);
+		} else {
+			i2c_set_ack(true);
+		}
+	}
+	i2c_master_stop();
+	x = (temp[1] << 8 | temp[0]);
+	y = (temp[3] << 8 | temp[2]);
+	z = (temp[5] << 8 | temp[4]);
+	printf("x:%d y:%d z:%d \n", x, y, z);   
+	sprintf(buf, "x:%d y:%d z:%d \n", x, y, z);
+	write(client_sock, buf, strlen(buf));
+}
+
+void write_to(int32 client_sock, uint8_t reg_addr, uint8_t val) {
+	uint8_t ack=1;
+	i2c_master_start();
+	//ack = i2c_master_writeByte((DEV_ADDR << 1) + 0);
+	i2c_master_writeByte((DEV_ADDR << 1) + 0);
+	if (!ack) {
+		printf("addr not ack when tx write command \n");
+		i2c_master_stop();
+	}	
+	os_delay_us(40000);
+	//ack = i2c_master_writeByte(reg_addr);
+	i2c_master_writeByte(reg_addr);
+	if (!ack) {
+		printf("reg addr not ack when tx write command \n");
+		i2c_master_stop();
+	}	
+	os_delay_us(40000);
+	//ack = i2c_master_writeByte(val);
+        i2c_master_writeByte(val);
+	if (!ack) {
+		printf("val not ack when tx write command \n");
+		i2c_master_stop();
+	}
+	i2c_master_stop();
+}
+
+void adxl_init() {
+	int response;
+	i2c_master_init();
+	// read device id.
+	read_reg(&response, 0x00);
+	printf("device: %x\n", response);
+}
+
+
+
+
+LOCAL void ICACHE_FLASH_ATTR LEDBlinkTask(void *pvParam) {
+  while(1) {
+    vTaskDelay(1000/portTICK_RATE_MS);
+    printf("Blink...\n");
+    if (GPIO_REG_READ(GPIO_OUT_ADDRESS) & BIT2)
+      {
+	// set gpio low
+	gpio_output_set(0,BIT2,BIT2,0);
+      }
+    else
+      {
+	// set gpio high
+	gpio_output_set(BIT2,0,BIT2,0);
+      }
+  }
+}
+
+
+
+
 /******************************************************************************
  * FunctionName : user_init
  * Description  : entry of user application, init user function here
@@ -89,5 +231,15 @@ void user_init(void)
   printf("* OpenSeizureDetector - ESP8266 Version *\n");
   printf("SDK version:%s\n", system_get_sdk_version());
   printf("*****************************************\n\n");
+
+
+  adxl_init();
+  
+   // Config pin as GPIO12
+  PIN_FUNC_SELECT (PERIPHS_IO_MUX_MTDI_U, FUNC_GPIO12);
+
+  xTaskCreate(LEDBlinkTask,(signed char *)"Blink",256,NULL,2,NULL);
+
+
 }
 
