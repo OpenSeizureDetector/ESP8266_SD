f614ad1f8758161b3212c095774c46379209d4c6
Graham Jones <grahamjones139@gmail.com>
2017-06-19 22:58:12 +0100

    Added ADXL345 library code based on Arduino library - it compiles but is not tested.

--- osd_app/adxl345/adxl345.c
+++ osd_app/adxl345/adxl345.c
@@ -1,59 +1,104 @@
 /*
- * ADXL345 Library
- */
+ *  ADXL345.c - ADXL345 library for freeRTOS.  Based on 
+ * Based on the Arduino ADXL345 library at 
+ *    https://github.com/jarzebski/Arduino-ADXL345
+ * (c) 2014 Korneliusz Jarzebski (www.jarzebski.pl)
+ * Copyright Graham Jones, 2017.
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the version 3 GNU General Public License as
+ published by the Free Software Foundation.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include <stdio.h>
-//#include "espressif/esp_common.h"
-//#include "../i2c/i2c.h"
 #include "adxl345.h"
 
+// Module variables - global within this module.
 uint8_t ADXL345_devAddr = 0xff;  // 7 bit i2c address range so this is off scale to
                          //    show  it is not initialised yet.
-//uint8_t ADXL345_SCL = 0xff;
-//uint8_t ADXL345_SDA = 0xff;
+ADXL345_Vector f;
 
 
-/* Reads a single byte from the ADXL345 at address devAddr, register regAddr
+
+/*****************************************
+ * Basic register input/output functions *
+ *****************************************/
+// Write byte to register
+uint8_t ADXL345_writeRegister8(uint8_t reg, uint8_t value)
+{
+  if (i2c_slave_read(ADXL345_devAddr, &reg, &value, 1)) {
+    // non-zero response = error
+    //printf("ADXL345_writeRegister8(0x%x, 0x%x) - Error\n",devAddr,regAddr);
+    return -1;
+  } else {
+    return value;
+  }
+
+}
+
+
+/* Reads a single byte from the ADXL345 register reg
  * @return: the byte read, or -1 on error.
  */
-uint8_t i2c_readRegister8(uint8_t regAddr) {
+uint8_t ADXL345_readRegister8(uint8_t reg) {
   uint8_t byte;
-  if (i2c_slave_read(ADXL345_devAddr, &regAddr, &byte, 1)) {
+  if (i2c_slave_read(ADXL345_devAddr, &reg, &byte, 1)) {
     // non-zero response = error
-    //printf("i2c_readRegister8(0x%x, 0x%x) - Error\n",devAddr,regAddr);
+    //printf("ADXL345_readRegister8(0x%x, 0x%x) - Error\n",devAddr,regAddr);
     return -1;
   } else {
     return byte;
   }
 }
 
+
 /* Reads a 2 byte word from the ADXL345 at address devAddr, register regAddr
  * @return: the word read, or -1 on error.
  */
-uint16_t i2c_readRegister16(uint8_t regAddr) {
-  uint16_t data;
-  if (i2c_slave_read(ADXL345_devAddr, &regAddr, (uint8_t*)&data, 2)) {
+uint16_t ADXL345_readRegister16(uint8_t reg) {
+  uint16_t value;
+  if (i2c_slave_read(ADXL345_devAddr, &reg, (uint8_t*)&value, 2)) {
     // non-zero response = error
-    //printf("i2c_readRegister8(0x%x, 0x%x) - Error\n",devAddr,regAddr);
+    //printf("ADXL345_readRegister8(0x%x, 0x%x) - Error\n",devAddr,reg);
     return -1;
   } else {
-    return data;
+    return value;
   }
 }
 
 
-/* Writes a single byte t the ADXL345 at address devAddr, register regAddr
- * @return: 0 on success, or -1 on error.
- */
-uint8_t i2c_writeRegister8(uint8_t regAddr, uint8_t byte) {
-  if (i2c_slave_write(ADXL345_devAddr, &regAddr, &byte, 1)) {
-    // non-zero response = error
-    //printf("i2c_writeRegister8(0x%x, 0x%x) - Error\n",devAddr,regAddr);
-    return -1;
-  } else {
-    return 0;
-  }
+void ADXL345_writeRegisterBit(uint8_t reg, uint8_t pos, bool state)
+{
+    uint8_t value;
+    value = ADXL345_readRegister8(reg);
+
+    if (state)
+    {
+	value |= (1 << pos);
+    } else 
+    {
+	value &= ~(1 << pos);
+    }
+
+    ADXL345_writeRegister8(reg, value);
 }
+
+bool ADXL345_readRegisterBit(uint8_t reg, uint8_t pos)
+{
+    uint8_t value;
+    value = ADXL345_readRegister8(reg);
+    return ((value >> pos) & 1);
+}
+
+
 
 
 /**
@@ -66,7 +111,7 @@ uint8_t ADXL345_findDevice() {
   uint8_t i;
   for (i=0;i<127;i++) {
     ADXL345_devAddr = i;
-    id = i2c_readRegister8(ADXL345_REG_DEVID);
+    id = ADXL345_readRegister8(ADXL345_REG_DEVID);
     if (id!=0xff) {
       printf("Response received from address %d (0x%x) - id=%d (0x%x)\n",i,i,id,id);
       if ((i==ADXL345_ADDRESS) || (i==ADXL345_ALT_ADDRESS)) {
@@ -95,23 +140,27 @@ uint8_t ADXL345_init(int scl, int sda) {
   printf("ADXL345 found at address %x - initialising it for measurement\n",
 	 ADXL345_devAddr);
   
-  if (i2c_writeRegister8(ADXL345_REG_POWER_CTL,0x08)) {
+  if (ADXL345_writeRegister8(ADXL345_REG_POWER_CTL,0x08)) {
     printf("Error setting measurement Mode\n");
   } else {
     printf("Measurement Mode set\n");
   }
+
+  f.XAxis = 0;
+  f.YAxis = 0;
+  f.ZAxis = 0;
+
   
   printf("ADXL345_init() complete\n");
   return(ADXL345_devAddr);
 }
 
 
-
 /* read the X axis acceleration */
 uint16_t ADXL345_getXAcc() {
   uint16_t xAcc;
-  printf("adxl345_getXacc\n");
-  if ((xAcc = i2c_readRegister8(ADXL345_REG_DATAX0))==0xff) {
+  printf("ADXL345_getXacc\n");
+  if ((xAcc = ADXL345_readRegister8(ADXL345_REG_DATAX0))==0xff) {
     printf("Error reading accel\n");
     return -1;
   } else {
@@ -121,13 +170,451 @@ uint16_t ADXL345_getXAcc() {
 }
 
 
+
+
+
+/*bool ADXL345_begin()
+{
+    f.XAxis = 0;
+    f.YAxis = 0;
+    f.ZAxis = 0;
+
+
+    // Check ADXL345 REG DEVID
+    if (ADXL345_readRegister8(ADXL345_REG_DEVID) != 0xE5)
+    {
+        return false;
+    }
+
+    // Enable measurement mode (0b00001000)
+    ADXL345_writeRegister8(ADXL345_REG_POWER_CTL, 0x08);
+
+    clearSettings();
+
+    return true;
+    }*/
+
+// Set Range
+void ADXL345_setRange(ADXL345_range_t range)
+{
+  // Get actual value register
+  uint8_t value = ADXL345_readRegister8(ADXL345_REG_DATA_FORMAT);
+
+  // Update the data rate
+  // (&) 0b11110000 (0xF0 - Leave HSB)
+  // (|) 0b0000xx?? (range - Set range)
+  // (|) 0b00001000 (0x08 - Set Full Res)
+  value &= 0xF0;
+  value |= range;
+  value |= 0x08;
+
+  ADXL345_writeRegister8(ADXL345_REG_DATA_FORMAT, value);
+}
+
+// Get Range
+ADXL345_range_t ADXL345_getRange(void)
+{
+    return (ADXL345_range_t)(ADXL345_readRegister8(ADXL345_REG_DATA_FORMAT) & 0x03);
+}
+
+// Set Data Rate
+void ADXL345_setDataRate(ADXL345_dataRate_t dataRate)
+{
+    ADXL345_writeRegister8(ADXL345_REG_BW_RATE, dataRate);
+}
+
+// Get Data Rate
+ADXL345_dataRate_t ADXL345_getDataRate(void)
+{
+    return (ADXL345_dataRate_t)(ADXL345_readRegister8(ADXL345_REG_BW_RATE) & 0x0F);
+}
+
+// Low Pass Filter
+ADXL345_Vector ADXL345_lowPassFilter(ADXL345_Vector vector, float alpha)
+{
+    f.XAxis = vector.XAxis * alpha + (f.XAxis * (1.0 - alpha));
+    f.YAxis = vector.YAxis * alpha + (f.YAxis * (1.0 - alpha));
+    f.ZAxis = vector.ZAxis * alpha + (f.ZAxis * (1.0 - alpha));
+    return f;
+}
+
 // Read raw values
 ADXL345_Vector ADXL345_readRaw(void)
 {
   ADXL345_Vector r;
-
-  r.XAxis = i2c_readRegister16(ADXL345_REG_DATAX0);
-  r.YAxis = i2c_readRegister16(ADXL345_REG_DATAY0);
-  r.ZAxis = i2c_readRegister16(ADXL345_REG_DATAZ0);
+  r.XAxis = ADXL345_readRegister16(ADXL345_REG_DATAX0);
+  r.YAxis = ADXL345_readRegister16(ADXL345_REG_DATAY0);
+  r.ZAxis = ADXL345_readRegister16(ADXL345_REG_DATAZ0);
   return r;
 }
+
+// Read normalized values
+ADXL345_Vector ADXL345_readNormalize(float gravityFactor)
+{
+  ADXL345_Vector r;
+  ADXL345_Vector n;
+  r = ADXL345_readRaw();
+
+  // (4 mg/LSB scale factor in Full Res) * gravity factor
+  n.XAxis = r.XAxis * 0.004 * gravityFactor;
+  n.YAxis = r.YAxis * 0.004 * gravityFactor;
+  n.ZAxis = r.ZAxis * 0.004 * gravityFactor;
+  
+  return n;
+}
+
+// Read scaled values
+ADXL345_Vector ADXL345_readScaled(void)
+{
+  ADXL345_Vector r;
+  ADXL345_Vector n;
+  r = ADXL345_readRaw();
+
+  // (4 mg/LSB scale factor in Full Res)
+  n.XAxis = r.XAxis * 0.004;
+  n.YAxis = r.YAxis * 0.004;
+  n.ZAxis = r.ZAxis * 0.004;
+  
+  return n;
+}
+
+void ADXL345_clearSettings(void)
+{
+    ADXL345_setRange(ADXL345_RANGE_2G);
+    ADXL345_setDataRate(ADXL345_DATARATE_100HZ);
+
+    ADXL345_writeRegister8(ADXL345_REG_THRESH_TAP, 0x00);
+    ADXL345_writeRegister8(ADXL345_REG_DUR, 0x00);
+    ADXL345_writeRegister8(ADXL345_REG_LATENT, 0x00);
+    ADXL345_writeRegister8(ADXL345_REG_WINDOW, 0x00);
+    ADXL345_writeRegister8(ADXL345_REG_THRESH_ACT, 0x00);
+    ADXL345_writeRegister8(ADXL345_REG_THRESH_INACT, 0x00);
+    ADXL345_writeRegister8(ADXL345_REG_TIME_INACT, 0x00);
+    ADXL345_writeRegister8(ADXL345_REG_THRESH_FF, 0x00);
+    ADXL345_writeRegister8(ADXL345_REG_TIME_FF, 0x00);
+
+    uint8_t value;
+
+    value = ADXL345_readRegister8(ADXL345_REG_ACT_INACT_CTL);
+    value &= 0b10001000;
+    ADXL345_writeRegister8(ADXL345_REG_ACT_INACT_CTL, value);
+
+    value = ADXL345_readRegister8(ADXL345_REG_TAP_AXES);
+    value &= 0b11111000;
+    ADXL345_writeRegister8(ADXL345_REG_TAP_AXES, value);
+}
+
+// Set Tap Threshold (62.5mg / LSB)
+void ADXL345_setTapThreshold(float threshold)
+{
+  // FIXME - add constrain() function to avoid over-runs.
+  //uint8_t scaled = constrain(threshold / 0.0625f, 0, 255);
+  uint8_t scaled = threshold / 0.0625f;
+  ADXL345_writeRegister8(ADXL345_REG_THRESH_TAP, scaled);
+}
+
+// Get Tap Threshold (62.5mg / LSB)
+float ADXL345_getTapThreshold(void)
+{
+    return ADXL345_readRegister8(ADXL345_REG_THRESH_TAP) * 0.0625f;
+}
+
+// Set Tap Duration (625us / LSB)
+void ADXL345_setTapDuration(float duration)
+{
+  // FIXME - add constrain() function to avoid over-runs.
+  //  uint8_t scaled = constrain(duration / 0.000625f, 0, 255);
+    uint8_t scaled = duration / 0.000625f;
+    ADXL345_writeRegister8(ADXL345_REG_DUR, scaled);
+}
+
+// Get Tap Duration (625us / LSB)
+float ADXL345_getTapDuration(void)
+{
+    return ADXL345_readRegister8(ADXL345_REG_DUR) * 0.000625f;
+}
+
+// Set Double Tap Latency (1.25ms / LSB)
+void ADXL345_setDoubleTapLatency(float latency)
+{
+  // FIXME - add constrain() function to avoid over-runs.
+  //  uint8_t scaled = constrain(latency / 0.00125f, 0, 255);
+  uint8_t scaled = latency / 0.00125f;
+  ADXL345_writeRegister8(ADXL345_REG_LATENT, scaled);
+}
+
+// Get Double Tap Latency (1.25ms / LSB)
+float ADXL345_getDoubleTapLatency()
+{
+    return ADXL345_readRegister8(ADXL345_REG_LATENT) * 0.00125f;
+}
+
+// Set Double Tap Window (1.25ms / LSB)
+void ADXL345_setDoubleTapWindow(float window)
+{
+  // FIXME - add constrain() function to avoid over-runs.
+  //  uint8_t scaled = constrain(window / 0.00125f, 0, 255);
+    uint8_t scaled = window / 0.00125f;
+    ADXL345_writeRegister8(ADXL345_REG_WINDOW, scaled);
+}
+
+// Get Double Tap Window (1.25ms / LSB)
+float ADXL345_getDoubleTapWindow(void)
+{
+    return ADXL345_readRegister8(ADXL345_REG_WINDOW) * 0.00125f;
+}
+
+// Set Activity Threshold (62.5mg / LSB)
+void ADXL345_setActivityThreshold(float threshold)
+{
+  // FIXME - add constrain() function to avoid over-runs.
+  //  uint8_t scaled = constrain(threshold / 0.0625f, 0, 255);
+    uint8_t scaled = threshold / 0.0625f;
+    ADXL345_writeRegister8(ADXL345_REG_THRESH_ACT, scaled);
+}
+
+// Get Activity Threshold (65.5mg / LSB)
+float ADXL345_getActivityThreshold(void)
+{
+    return ADXL345_readRegister8(ADXL345_REG_THRESH_ACT) * 0.0625f;
+}
+
+// Set Inactivity Threshold (65.5mg / LSB)
+void ADXL345_setInactivityThreshold(float threshold)
+{
+  // FIXME - add constrain() function to avoid over-runs.
+  //  uint8_t scaled = constrain(threshold / 0.0625f, 0, 255);
+  uint8_t scaled = threshold / 0.0625f;
+  ADXL345_writeRegister8(ADXL345_REG_THRESH_INACT, scaled);
+}
+
+// Get Incactivity Threshold (65.5mg / LSB)
+float ADXL345_getInactivityThreshold(void)
+{
+    return ADXL345_readRegister8(ADXL345_REG_THRESH_INACT) * 0.0625f;
+}
+
+// Set Inactivity Time (s / LSB)
+void ADXL345_setTimeInactivity(uint8_t time)
+{
+    ADXL345_writeRegister8(ADXL345_REG_TIME_INACT, time);
+}
+
+// Get Inactivity Time (s / LSB)
+uint8_t ADXL345_getTimeInactivity(void)
+{
+    return ADXL345_readRegister8(ADXL345_REG_TIME_INACT);
+}
+
+// Set Free Fall Threshold (65.5mg / LSB)
+void ADXL345_setFreeFallThreshold(float threshold)
+{
+  // FIXME - add constrain() function to avoid over-runs.
+  //  uint8_t scaled = constrain(threshold / 0.0625f, 0, 255);
+  uint8_t scaled = threshold / 0.0625f;
+  ADXL345_writeRegister8(ADXL345_REG_THRESH_FF, scaled);
+}
+
+// Get Free Fall Threshold (65.5mg / LSB)
+float ADXL345_getFreeFallThreshold(void)
+{
+    return ADXL345_readRegister8(ADXL345_REG_THRESH_FF) * 0.0625f;
+}
+
+// Set Free Fall Duratiom (5ms / LSB)
+void ADXL345_setFreeFallDuration(float duration)
+{
+  // FIXME - add constrain() function to avoid over-runs.
+  //uint8_t scaled = constrain(duration / 0.005f, 0, 255);
+  uint8_t scaled = duration / 0.005f;
+  ADXL345_writeRegister8(ADXL345_REG_TIME_FF, scaled);
+}
+
+// Get Free Fall Duratiom
+float ADXL345_getFreeFallDuration()
+{
+    return ADXL345_readRegister8(ADXL345_REG_TIME_FF) * 0.005f;
+}
+
+void ADXL345_setActivityX(bool state)
+{
+    ADXL345_writeRegisterBit(ADXL345_REG_ACT_INACT_CTL, 6, state);
+}
+
+bool ADXL345_getActivityX(void)
+{
+    return ADXL345_readRegisterBit(ADXL345_REG_ACT_INACT_CTL, 6);
+}
+
+void ADXL345_setActivityY(bool state)
+{
+    ADXL345_writeRegisterBit(ADXL345_REG_ACT_INACT_CTL, 5, state);
+}
+
+bool ADXL345_getActivityY(void)
+{
+    return ADXL345_readRegisterBit(ADXL345_REG_ACT_INACT_CTL, 5);
+}
+
+void ADXL345_setActivityZ(bool state)
+{
+    ADXL345_writeRegisterBit(ADXL345_REG_ACT_INACT_CTL, 4, state);
+}
+
+bool ADXL345_getActivityZ(void)
+{
+    return ADXL345_readRegisterBit(ADXL345_REG_ACT_INACT_CTL, 4);
+}
+
+void ADXL345_setActivityXYZ(bool state)
+{
+    uint8_t value;
+
+    value = ADXL345_readRegister8(ADXL345_REG_ACT_INACT_CTL);
+
+    if (state)
+    {
+	value |= 0b00111000;
+    } else
+    {
+	value &= 0b11000111;
+    }
+
+    ADXL345_writeRegister8(ADXL345_REG_ACT_INACT_CTL, value);
+}
+
+
+void ADXL345_setInactivityX(bool state) 
+{
+    ADXL345_writeRegisterBit(ADXL345_REG_ACT_INACT_CTL, 2, state);
+}
+
+bool ADXL345_getInactivityX(void)
+{
+    return ADXL345_readRegisterBit(ADXL345_REG_ACT_INACT_CTL, 2);
+}
+
+void ADXL345_setInactivityY(bool state)
+{
+    ADXL345_writeRegisterBit(ADXL345_REG_ACT_INACT_CTL, 1, state);
+}
+
+bool ADXL345_getInactivityY(void)
+{
+    return ADXL345_readRegisterBit(ADXL345_REG_ACT_INACT_CTL, 1);
+}
+
+void ADXL345_setInactivityZ(bool state)
+{
+    ADXL345_writeRegisterBit(ADXL345_REG_ACT_INACT_CTL, 0, state);
+}
+
+bool ADXL345_getInactivityZ(void)
+{
+    return ADXL345_readRegisterBit(ADXL345_REG_ACT_INACT_CTL, 0);
+}
+
+void ADXL345_setInactivityXYZ(bool state)
+{
+    uint8_t value;
+
+    value = ADXL345_readRegister8(ADXL345_REG_ACT_INACT_CTL);
+
+    if (state)
+    {
+	value |= 0b00000111;
+    } else
+    {
+	value &= 0b11111000;
+    }
+
+    ADXL345_writeRegister8(ADXL345_REG_ACT_INACT_CTL, value);
+}
+
+void ADXL345_setTapDetectionX(bool state)
+{
+    ADXL345_writeRegisterBit(ADXL345_REG_TAP_AXES, 2, state);
+}
+
+bool ADXL345_getTapDetectionX(void)
+{
+    return ADXL345_readRegisterBit(ADXL345_REG_TAP_AXES, 2);
+}
+
+void ADXL345_setTapDetectionY(bool state)
+{
+    ADXL345_writeRegisterBit(ADXL345_REG_TAP_AXES, 1, state);
+}
+
+bool ADXL345_getTapDetectionY(void)
+{
+    return ADXL345_readRegisterBit(ADXL345_REG_TAP_AXES, 1);
+}
+
+void ADXL345_setTapDetectionZ(bool state)
+{
+    ADXL345_writeRegisterBit(ADXL345_REG_TAP_AXES, 0, state);
+}
+
+bool ADXL345_getTapDetectionZ(void)
+{
+    return ADXL345_readRegisterBit(ADXL345_REG_TAP_AXES, 0);
+}
+
+void ADXL345_setTapDetectionXYZ(bool state)
+{
+    uint8_t value;
+
+    value = ADXL345_readRegister8(ADXL345_REG_TAP_AXES);
+
+    if (state)
+    {
+	value |= 0b00000111;
+    } else
+    {
+	value &= 0b11111000;
+    }
+
+    ADXL345_writeRegister8(ADXL345_REG_TAP_AXES, value);
+}
+
+
+void ADXL345_useInterrupt(ADXL345_int_t interrupt)
+{
+    if (interrupt == 0)
+    {
+	ADXL345_writeRegister8(ADXL345_REG_INT_MAP, 0x00);
+    } else
+    {
+	ADXL345_writeRegister8(ADXL345_REG_INT_MAP, 0xFF);
+    }
+
+    ADXL345_writeRegister8(ADXL345_REG_INT_ENABLE, 0xFF);
+}
+
+Activites ADXL345_readActivites(void)
+{
+  Activites a;
+  uint8_t data = ADXL345_readRegister8(ADXL345_REG_INT_SOURCE);
+  
+  a.isOverrun = ((data >> ADXL345_OVERRUN) & 1);
+  a.isWatermark = ((data >> ADXL345_WATERMARK) & 1);
+  a.isFreeFall = ((data >> ADXL345_FREE_FALL) & 1);
+  a.isInactivity = ((data >> ADXL345_INACTIVITY) & 1);
+  a.isActivity = ((data >> ADXL345_ACTIVITY) & 1);
+  a.isDoubleTap = ((data >> ADXL345_DOUBLE_TAP) & 1);
+  a.isTap = ((data >> ADXL345_SINGLE_TAP) & 1);
+  a.isDataReady = ((data >> ADXL345_DATA_READY) & 1);
+  
+  data = ADXL345_readRegister8(ADXL345_REG_ACT_TAP_STATUS);
+  
+  a.isActivityOnX = ((data >> 6) & 1);
+  a.isActivityOnY = ((data >> 5) & 1);
+  a.isActivityOnZ = ((data >> 4) & 1);
+  a.isTapOnX = ((data >> 2) & 1);
+  a.isTapOnY = ((data >> 1) & 1);
+  a.isTapOnZ = ((data >> 0) & 1);
+  
+  return a;
+}
+
--- osd_app/adxl345/adxl345.h
+++ osd_app/adxl345/adxl345.h
@@ -1,13 +1,34 @@
 /*
- * ADXL345 RTOS Library Header File
+ * ADXL345 FreeRTOS Library Header File
  * Based on the Arduino ADXL345 library at 
  *    https://github.com/jarzebski/Arduino-ADXL345
+ * (c) 2014 Korneliusz Jarzebski (www.jarzebski.pl)
+ * Copyright Graham Jones, 2017.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the version 3 GNU General Public License as
+ * published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
+
 #include "espressif/esp_common.h"
 #include "../i2c/i2c.h"
 
 
+
+
+
+
+#ifndef ADXL345_h
+#define ADXL345_h
 
 #define ADXL345_ADDRESS              (0x1D)
 #define ADXL345_ALT_ADDRESS          (0x53)
@@ -45,18 +66,173 @@
 #define ADXL345_REG_FIFO_CTL         (0x38)
 #define ADXL345_REG_FIFO_STATUS      (0x39)
 
+#define ADXL345_GRAVITY_SUN          273.95f
+#define ADXL345_GRAVITY_EARTH        9.80665f
+#define ADXL345_GRAVITY_MOON         1.622f
+#define ADXL345_GRAVITY_MARS         3.69f
+#define ADXL345_GRAVITY_NONE         1.00f
 
 #ifndef VECTOR_STRUCT_H
 #define VECTOR_STRUCT_H
 typedef struct 
 {
-    float XAxis;
-    float YAxis;
-    float ZAxis;
+  float XAxis;
+  float YAxis;
+  float ZAxis;
 } ADXL345_Vector;
 #endif
+
+typedef enum
+{
+    ADXL345_DATARATE_3200HZ    = 0b1111,
+    ADXL345_DATARATE_1600HZ    = 0b1110,
+    ADXL345_DATARATE_800HZ     = 0b1101,
+    ADXL345_DATARATE_400HZ     = 0b1100,
+    ADXL345_DATARATE_200HZ     = 0b1011,
+    ADXL345_DATARATE_100HZ     = 0b1010,
+    ADXL345_DATARATE_50HZ      = 0b1001,
+    ADXL345_DATARATE_25HZ      = 0b1000,
+    ADXL345_DATARATE_12_5HZ    = 0b0111,
+    ADXL345_DATARATE_6_25HZ    = 0b0110,
+    ADXL345_DATARATE_3_13HZ    = 0b0101,
+    ADXL345_DATARATE_1_56HZ    = 0b0100,
+    ADXL345_DATARATE_0_78HZ    = 0b0011,
+    ADXL345_DATARATE_0_39HZ    = 0b0010,
+    ADXL345_DATARATE_0_20HZ    = 0b0001,
+    ADXL345_DATARATE_0_10HZ    = 0b0000
+} ADXL345_dataRate_t;
+
+typedef enum
+{
+    ADXL345_INT2 = 0b01,
+    ADXL345_INT1 = 0b00
+} ADXL345_int_t;
+
+typedef enum
+{
+    ADXL345_DATA_READY         = 0x07,
+    ADXL345_SINGLE_TAP         = 0x06,
+    ADXL345_DOUBLE_TAP         = 0x05,
+    ADXL345_ACTIVITY           = 0x04,
+    ADXL345_INACTIVITY         = 0x03,
+    ADXL345_FREE_FALL          = 0x02,
+    ADXL345_WATERMARK          = 0x01,
+    ADXL345_OVERRUN            = 0x00
+} ADXL345_activity_t;
+
+typedef enum
+{
+    ADXL345_RANGE_16G          = 0b11,
+    ADXL345_RANGE_8G           = 0b10,
+    ADXL345_RANGE_4G           = 0b01,
+    ADXL345_RANGE_2G           = 0b00
+} ADXL345_range_t;
+
+
+typedef struct
+{
+    bool isOverrun;
+    bool isWatermark;
+    bool isFreeFall;
+    bool isInactivity;
+    bool isActivity;
+    bool isActivityOnX;
+    bool isActivityOnY;
+    bool isActivityOnZ;
+    bool isDoubleTap;
+    bool isTap;
+    bool isTapOnX;
+    bool isTapOnY;
+    bool isTapOnZ;
+    bool isDataReady;
+} Activites;
+
+bool begin(void);
+void clearSettings(void);
+
+ADXL345_Vector readRaw(void);
+ADXL345_Vector readNormalize(float gravityFactor);
+ADXL345_Vector readScaled(void);
+
+Activites readActivites(void);
+
+ADXL345_Vector lowPassFilter(ADXL345_Vector vector, float alpha);
+
+void  setRange(ADXL345_range_t range);
+ADXL345_range_t getRange(void);
+
+void  setDataRate(ADXL345_dataRate_t dataRate);
+ADXL345_dataRate_t getDataRate(void);
+
+void setTapThreshold(float threshold);
+float getTapThreshold(void);
+
+void setTapDuration(float duration);
+float getTapDuration(void);
+
+void setDoubleTapLatency(float latency);
+float getDoubleTapLatency(void);
+
+void setDoubleTapWindow(float window);
+float getDoubleTapWindow(void);
+
+void setActivityThreshold(float threshold);
+float getActivityThreshold(void);
+
+void setInactivityThreshold(float threshold);
+float getInactivityThreshold(void);
+
+void setTimeInactivity(uint8_t time);
+uint8_t getTimeInactivity(void);
+
+void setFreeFallThreshold(float threshold);
+float getFreeFallThreshold(void);
+
+void setFreeFallDuration(float duration);
+float getFreeFallDuration();
+
+void setActivityX(bool state);
+bool getActivityX(void);
+void setActivityY(bool state);
+bool getActivityY(void);
+void setActivityZ(bool state);
+bool getActivityZ(void);
+void setActivityXYZ(bool state);
+
+void setInactivityX(bool state);
+bool getInactivityX(void);
+void setInactivityY(bool state);
+bool getInactivityY(void);
+void setInactivityZ(bool state);
+bool getInactivityZ(void);
+void setInactivityXYZ(bool state);
+
+void setTapDetectionX(bool state);
+bool getTapDetectionX(void);
+void setTapDetectionY(bool state);
+bool getTapDetectionY(void);
+void setTapDetectionZ(bool state);
+bool getTapDetectionZ(void);
+void setTapDetectionXYZ(bool state);
+
+void useInterrupt(ADXL345_int_t interrupt);
+
+
+void writeRegister8(uint8_t reg, uint8_t value);
+uint8_t readRegister8(uint8_t reg);
+uint8_t fastRegister8(uint8_t reg);
+int16_t readRegister16(uint8_t reg);
+void writeRegisterBit(uint8_t reg, uint8_t pos, bool state);
+bool readRegisterBit(uint8_t reg, uint8_t pos);
+
+
+
+
 
 // Function prototypes
 uint8_t ADXL345_init(int scl, int sca);
 uint16_t ADXL345_getXAcc();
 ADXL345_Vector ADXL345_readRaw();
+
+
+#endif
